5cf741b3a05f1554572c7a9d9ad6b4b68e037dd7
Jake Thornberry <jakethornberry@Rocinante.local>
2017-06-04 12:55:26 -0500

    Pseudocode app at basic level

---
+++ assets/javascript/app.js
@@ -0,0 +1,11 @@
+// Start button
+
+// When start button is clicked, timer starts countind down from 2:00 minutes
+
+// Load questions and answers
+
+// Multiple choice answers: user can select only one
+
+// When timer runs out (120 seconds?) game ends
+
+// Game over box with number of correct answers, incorrect answers, unanswered questions, and play again button
