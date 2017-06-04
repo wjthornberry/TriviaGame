84bec37e44246cc9e6811bbceb6b24d09e4e241e
Jake Thornberry <jakethornberry@Rocinante.local>
2017-06-04 14:02:07 -0500

    Add questions and answers to HTML; extra functionality in JS to display different characters depending on user's score

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -9,3 +9,15 @@
 // When timer runs out (120 seconds?) game ends
 
 // Game over box with number of correct answers, incorrect answers, unanswered questions, and play again button
+
+// Possible extra functionality if I have time: based on the user's score, assign a picture and text.
+
+    //Ex: Correct: x / Incorrect: x 
+    // 90-100%>"Congratulations, you're Michael Bluth, the man who knows everything 
+    //about his family...for better or for worse."
+
+    // 60-89%>: "You're Steve Holt!" Not the brightest, but what charisma!
+
+    // 0-59%: "You're GOB, Lucille's least favorite child."
+
+  
